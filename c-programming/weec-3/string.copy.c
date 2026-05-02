28273dd78775539457b87b5ee13dc12bc4939ada
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-05-02 18:18:30 +0600

    clea

--- c-programming/weec-3/string.copy.c
+++ c-programming/weec-3/string.copy.c
@@ -1,14 +1,24 @@
 #include<stdio.h>
+#include<string.h>
 
 
 int main(){
   
 
-  int name = 4 ;
+  // int name = 4 ;
 
-  for(int i=0; i<=5;i++){
-    printf("%d\n",i);
-  }
+  // for(int i=0; i<=5;i++){
+  //   printf("%d\n",i);
+  // }
+
+
+  char a[101],b[1001];
+  scanf("%s %s",a,b);
+
+  strcpy(a,b);
+
+  printf("%s %s", a ,b);
+
   
   return 0;
 }
