2bb723c9b5fe90fee98ca5947736ddeee13c81d3
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-05-04 16:25:43 +0600

    sum of two value equal

---
+++ c-programming/weec-3/sum_of_two_value_equal_x.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+
+int main(){
+  
+  // input value of array size ;
+  int n ; // array size number 
+  scanf("%d" , &n);
+
+
+  int a[n]; // array size value  //  array value is = [2,3,4,5,6]
+  for(int i=0; i< n ;i++){
+    scanf("%d",&a[i]);
+  }
+
+  // input value of x !
+  int x ;   //* target value is = 11
+  scanf("%d", &x);
+
+  // flag variable 
+  int flag = 0 ; 
+
+  for(int i = 0; i < n-1  /* n = 5-1 = 4 */ ;i++){
+    for( int j = i+1 ; j < n; j++ ){
+    //  printf("%d %d\n" ,   a[i],a[j]);
+
+    if(a[i]+a[j]==x){
+      flag=1;
+      printf("Yes\n");
+    }
+    }
+  }
+  
+  if(flag == 0){
+    printf("No");
+  }
+
+
+  return 0;
+}
