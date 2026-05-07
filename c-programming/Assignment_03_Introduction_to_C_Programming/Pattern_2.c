674449a5eaad7a6521bb85117660b47be2d3afe3
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-05-07 23:02:42 +0600

    solve pattern 2

---
+++ c-programming/Assignment_03_Introduction_to_C_Programming/Pattern_2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+  int N;
+  scanf("%d", &N);
+
+  for (int i = 1; i <= N; i++)
+  {
+
+    // Print leading spaces
+    for (int j = 1; j <= N - i; j++)
+    {
+      printf(" ");
+    }
+
+    // Print numbers in reverse order
+    for (int j = i; j >= 1; j--)
+    {
+      printf("%d", j);
+    }
+
+    printf("\n");
+  }
+
+  return 0;
+}
