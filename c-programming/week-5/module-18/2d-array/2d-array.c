b9236a5b18930a686588627b0d266891d1e6ee7e
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-05-13 00:03:27 +0600

    ..'

--- c-programming/week-5/module-18/2d-array/2d-array.c
+++ c-programming/week-5/module-18/2d-array/2d-array.c
@@ -1,10 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
+int main()
+{
 
-int main(){
-  
-int r,c;
-scanf("%d %d",&r,&c);
-int n[r][c];
-return 0;
+  int r, c;
+  scanf("%d %d", &r, &c);
+  // int a[r][c];
+
+  for (int i = 0; i < r; i++)
+  {
+
+    for (int j = 0; j < c; j++)
+    {
+      printf("%d %d", i, j);
+    }
+    printf("%d", i);
+    printf("\n");
+  }
+  return 0;
 }
