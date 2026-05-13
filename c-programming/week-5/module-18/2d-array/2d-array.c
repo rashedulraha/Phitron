e57e484092b5936d0b527f94e0e3a8a54f088668
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-05-13 07:49:51 +0600

    spacific column print

--- c-programming/week-5/module-18/2d-array/2d-array.c
+++ c-programming/week-5/module-18/2d-array/2d-array.c
@@ -1,21 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main()
-{
 
-  int r, c;
-  scanf("%d %d", &r, &c);
-  // int a[r][c];
+int main(){
+  
+int r,c;
+scanf("%d %d",&r,&c);
+int n[r][c];
 
-  for (int i = 0; i < r; i++)
-  {
 
-    for (int j = 0; j < c; j++)
-    {
-      printf("%d %d", i, j);
-    }
-    printf("%d", i);
-    printf("\n");
-  }
-  return 0;
+
+return 0;
 }
