0f2867e28eb881e7febf3933f9d668dc43f406f2
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-05-21 17:27:02 +0600

    ternary operator

---
+++ C++_For_DSA/week-1/module-1/if_else.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 9;
+
+    if(a%2==0){
+      cout << "Even";
+    }else{
+      cout << "Odd";
+    }
+    return 0;
+}
---
+++ C++_For_DSA/week-1/module-1/ternary_Oparetor.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x  = 5;
+
+  
+   ( x%2==0 )? cout<<"Even":cout<<"Odd";
+    return 0;
+}
