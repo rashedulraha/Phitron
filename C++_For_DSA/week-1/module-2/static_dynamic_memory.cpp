409beb508502bf62a198b1cf6672a9f8214f78c1
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-05-22 00:07:26 +0600

    dynamic memory

---
+++ C++_For_DSA/week-1/module-2/static_dynamic_memory.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // cout<< "Hello Name space";
+
+    // dynamic and static memory 
+
+    int *p = new int ; 
+    *p = 20; 
+     cout << *p<<endl;
+    return 0;
+}
