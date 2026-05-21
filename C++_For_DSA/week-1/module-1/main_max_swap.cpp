b6ef33b477612e67a2698ebf6902130ed85374d1
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-05-21 17:51:56 +0600

    input muliple number to print max and min number using c++ buildin function max and min

---
+++ C++_For_DSA/week-1/module-1/main_max_swap.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    int a,b ;
+    cin >> a >> b ;
+
+    // cout<< a << endl << b;
+
+    // fint the min number in  this two value 
+
+    // if(a<b){
+    //   cout << a<<endl;
+    // }else{
+    //   cout << b <<endl;
+    // }
+
+    //*print min number with built in function -> main
+
+    // cout << min(a,b) << endl;
+
+    //* max number print 
+    //  cout << max(a,b) << endl;
+
+
+    // * input multiple number to print max number using max function
+
+    cout << max({4,5,6,7,65,343,34})<< endl;
+    
+
+    // same way to using min function
+    cout << min({4,5,6,7,65,343,34})<< endl;
+
+
+
+
+
+
+
+    return 0;
+
+
+
+}
