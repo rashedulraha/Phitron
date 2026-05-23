7f46c719bd6d9da651613e57f142b44ac0883dc8
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-05-24 04:22:33 +0600

    adding statik  mmemory and dynamic memory"

--- C++_For_DSA/week-1/module-2/dynamic_array_from_function.cpp
+++ C++_For_DSA/week-1/module-2/dynamic_array_from_function.cpp
@@ -1,21 +1,42 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int* arr = new int[n]; // dynamic array
+
+//     for(int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     for(int i = 0; i < n; i++) {
+//         cout << arr[i] << " ";
+//     }
+
+//     delete[] arr; // memory free
+
+//     return 0;
+// }
+
+
+
 #include <iostream>
 using namespace std;
 
+int* func(){
+  int *a = new int [5];
+    for (int i=0;i<5;i++){
+      cin >> a[i];
+    }
+  return a;
+}
+
 int main() {
-    int n;
-    cin >> n;
-
-    int* arr = new int[n]; // dynamic array
-
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    for(int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-
-    delete[] arr; // memory free
-
-    return 0;
+   int * p =  func();
+     for (int i=0;i<5;i++){
+      cout << p[i] << " ";
+    }  
+  return 0;
 }
