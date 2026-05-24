dee887400430656d6fab385f959855bcb6c3930b
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-05-24 22:08:47 +0600

    declare and class and object

--- C++_For_DSA/week-1/module-3-class-object/declare_class_object.cpp
+++ C++_For_DSA/week-1/module-3-class-object/declare_class_object.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
 
+class Student{
+public:
+
+char name[100];
+int roll;
+double gpa;
+
+};
+
 int main() {
     
-
+Student a; 
+a.roll = 48; 
+a.gpa = 4.50;
+char temp [100]= "Nothing";
+strcpy(a.name,temp);
+cout << a.name << " " << a.roll << " " << a.gpa;
     return 0;
 }
