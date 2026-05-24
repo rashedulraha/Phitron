a568fb794cdecd09ecd2b2c7ba622fb2ea9a1e29
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-05-24 09:49:34 +0600

    simple sumation

---
+++ C++_For_DSA/Problem_solviing/week-1/simple_calculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+void func(int a,int b){
+
+
+ cout << a << " " << "+" << " " << b << " " <<"="  << " " << a+b << endl;
+ cout << a << " " << "*" << " " << b << " " <<"="  << " " << a*b << endl;
+
+ if (a>b){
+  cout << a << " " << "-" << " " << b << " " <<"="  << " " << a-b << endl;
+
+ }else{
+  cout << "subtraction failed";
+ }
+
+}
+int main() {
+    func(9,4);
+    return 0;
+}
