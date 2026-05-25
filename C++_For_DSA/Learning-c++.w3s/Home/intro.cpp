70859f69a660281175803d46de2da572639b0e04
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-05-25 22:46:22 +0600

    learnig multiple variable

--- C++_For_DSA/Learning-c++.w3s/Home/intro.cpp
+++ C++_For_DSA/Learning-c++.w3s/Home/intro.cpp
@@ -5,5 +5,45 @@ int main() {
 
   // cout << "Hello world check file";
   // cout << "Hello world";
+  // * print string
+  // cout << "I'm learning c++";
+  // * print number
+  // cout << 8 << endl;
+  // cout << 9 + 8;
+
+  //* c++ output
+
+  //* c++ new line
+
+  // cout << "i'm learning c++"
+  //      << "\n";
+  // cout << "Hello i'm a new line for this command"
+  //      << "\n";
+
+  // ? print name and age  ?
+
+  // cout << "Name : "
+  //      << "Rashedul islam"
+  //      << "\n";
+  // cout << "Age : " << 21 << "\n";
+
+  // int number = 8;
+  // double decimal = 54.4;
+  // char charter = 'H';
+  // string name = "Rashedul islam";
+  // bool trueFalse = true;
+
+  // cout << trueFalse;
+
+  // * using multiple variable declare 1 data types
+  // int x = 4, y = 5, z = 6;
+  // cout << z;
+
+  // * store multiple variable name  and  store one data ;
+
+  // int x, t, y;
+  // x = t = y = 40; // y=40, t=y/40, z=t/40;
+  // cout << y + t;
+
   return 0;
 }
--- C++_For_DSA/Problem_solviing/week-1/simple_calculator.cpp
+++ C++_For_DSA/Problem_solviing/week-1/simple_calculator.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 using namespace std;
 
+void func(int a, int b) {
 
-void func(int a,int b){
+  cout << a << " "
+       << "+"
+       << " " << b << " "
+       << "="
+       << " " << a + b << endl;
+  cout << a << " "
+       << "*"
+       << " " << b << " "
+       << "="
+       << " " << a * b << endl;
 
+  if (a > b) {
+    cout << a << " "
+         << "-"
+         << " " << b << " "
+         << "="
+         << " " << a - b << endl;
 
- cout << a << " " << "+" << " " << b << " " <<"="  << " " << a+b << endl;
- cout << a << " " << "*" << " " << b << " " <<"="  << " " << a*b << endl;
-
- if (a>b){
-  cout << a << " " << "-" << " " << b << " " <<"="  << " " << a-b << endl;
-
- }else{
-  cout << "subtraction failed";
- }
-
+  } else {
+    cout << "subtraction failed";
+  }
 }
 int main() {
-    func(9,4);
-    return 0;
+  func(9, 4);
+  return 0;
 }
