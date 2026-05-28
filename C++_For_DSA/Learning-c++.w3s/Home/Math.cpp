967c1276f52da414d728d835a345fed41505ba6e
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-05-29 00:43:19 +0600

    learning math  and using cmath libary

---
+++ C++_For_DSA/Learning-c++.w3s/Home/Math.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+  // * max and min
+  // cout << max(4, 5) << endl;
+  // cout << min(4, 5);
+
+  // * cmath libary
+  cout << sqrt(64) << endl;
+  //  round
+  cout << round(3.4) << endl;
+  cout << log(100);
+  return 0;
+}
