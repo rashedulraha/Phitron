67792a9c13b05e82f33af0d0064e5a9688714488
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-05-30 09:19:13 +0600

    break and continue

---
+++ C++_For_DSA/Learning-c++.w3s/Home/break_continue.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // cout << "Hello world";
+
+  // * break and continue  ;
+
+  for (int i = 0; i <= 4; i++) {
+
+    // if (i == 2) {
+    //   continue;
+    // }
+
+    // if (i == 3) {
+    //   break;
+    // }
+
+    // cout << i << endl;
+  }
+  return 0;
+}
