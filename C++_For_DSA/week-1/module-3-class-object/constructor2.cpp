e908c6f1db38195ef7c60497070afe99d2554237
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-06-04 23:39:47 +0600

    learning constructor function and call and print  values

---
+++ C++_For_DSA/week-1/module-3-class-object/constructor2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Product
+{
+
+public:
+  string name;
+  string description;
+  int price;
+
+  Product(string name, string description, int price)
+  {
+    this->name = name;
+    this->description = description;
+    this->price = price;
+  };
+};
+
+int main()
+{
+
+  Product Ponds("Pond's", "This product is so great", 540);
+
+  cout << Ponds.name << endl
+       << Ponds.description << endl
+       << Ponds.price << endl;
+  return 0;
+}
