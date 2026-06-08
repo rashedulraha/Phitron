4f4399381e8ac68d5e752f21c981d3887dc76df1
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-06-08 18:34:04 +0600

    reverse function and for lop

---
+++ C++_For_DSA/week-1/module-6/reverse_function.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // reverse function
+
+  int arraySize;
+  cin >> arraySize;
+
+  int arrayInput[arraySize];
+
+  for (int i = 0; i < arraySize; i++) {
+    cin >> arrayInput[i];
+  }
+  // for (int i = 0; i < arraySize; i++) {
+  //   cout << arrayInput[i];
+  // }
+
+  return 0;
+}
