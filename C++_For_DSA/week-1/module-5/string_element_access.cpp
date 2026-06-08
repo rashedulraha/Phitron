8f43a770b20e91b813355b4775ee731c61285c0b
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-06-08 11:41:48 +0600

    string element access

---
+++ C++_For_DSA/week-1/module-5/string_element_access.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  // cout << s[1] << endl;
+
+  // print using method
+  // cout << s.at(0) << endl;
+
+  // print front and bank
+
+  // cout << s.front() << endl;
+  cout << s.back() << endl;        // same as
+  cout << s[s.size() - 1] << endl; // same ass
+
+  return 0;
+}
