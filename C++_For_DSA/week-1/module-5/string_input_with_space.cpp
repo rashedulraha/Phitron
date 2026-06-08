1c110ab29ede605fccdc7e73dcd0170cd1756a75
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-06-08 12:38:28 +0600

    input string with space

--- C++_For_DSA/week-1/module-5/string_input_with_space.cpp
+++ C++_For_DSA/week-1/module-5/string_input_with_space.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main() {
   // string input with space
+  int i;
+  string s;
 
+  cin >> i;
+  cout << i << endl;
+
+  // cin >> s;
+  // cout << s;
+  cin.ignore();
+  getline(cin, s);
+  cout << s;
   return 0;
 }
