c969afc2e4c02145540bdfada45fe269aba5b473
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-06-09 07:14:12 +0600

    learning function

---
+++ C++_For_DSA/Learning-c++.w3s/function/function.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// create function
+// void myFunction() { cout << "i just fo executed" << endl; };
+
+// * function declaration
+void myFunction(int a, int b) {} // declaration
+
+// * function definition
+void myFunctionDefinition() {
+  cout << "This function name definition";
+} // definition
+
+int main() {
+  //* learning function
+
+  return 0;
+}
